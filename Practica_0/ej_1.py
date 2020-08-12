02a3959057cfe8dc0156d707dde7fe8905dba1f3
Cabre <cabre94@hotmail.com>
2020-08-12 02:24:27 -0300

    Resuelvo el ejercicio, parece que no me acuerdo como multiplicar matrices jeje

--- Practica_0/ej_1.py
+++ Practica_0/ej_1.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+A = np.array([[ 1,  0,  1],
+			  [ 2, -1,  1],
+			  [-3,  2, -2]])
+
+b = np.array([[-2],
+			  [ 1],
+			  [-1]])
+
+c = np.array([-2, 1,-1])
+
+res = np.array([1, -2, -3])
+
+# x = 1  y = -2     z = -3
