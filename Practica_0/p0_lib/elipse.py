adf55e2f52d8a51d73b5a969fbbb31c0e63d51f2
Cabre <cabre94@hotmail.com>
2020-08-17 15:30:07 -0300

    Agrego paquete p0_lib con los modulos circunferencia.py, rectangulo.py, elipse.py y __init__.py. Todo del ejercicio 9

---
+++ Practica_0/p0_lib/elipse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+17-08-2020
+File: elipse.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def area(a,b):
+	return np.pi * a * b
