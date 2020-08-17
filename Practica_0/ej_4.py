3d91878f3ccf3b195d35ebb7d9dbfd551a758f61
Cabre <cabre94@hotmail.com>
2020-08-17 15:24:19 -0300

    Cambios menores

--- Practica_0/ej_4.py
+++ Practica_0/ej_4.py
@@ -22,10 +22,10 @@ def grafYRaices(a,b,c):
 	r1, r2 = Bhaskara(a,b,c)
 
 	plt.plot(x,p,'b-')
-	if r1.imag == 0 and r1 < 4 and r1 > -4.5:
+	if r1.imag == 0 and r1 <= 4 and r1 >= -4.5:
 		plt.plot(r1,0,'or')
 		plt.annotate(s=r"$x_1=${:.2f}".format(r1), xy=(r1, 0), xytext=(r1+0.3, 0))
-	if r2.imag == 0 and r2 < 4 and r2 > -4.5:
+	if r2.imag == 0 and r2 <= 4 and r2 >= -4.5:
 		plt.plot(r2,0,'or')
 		plt.annotate(s=r"$x_2=${:.2f}".format(r2), xy=(r2, 0), xytext=(r2+0.3, 0))
 	plt.show()
