9fe0d75d5d3199ae64f592ec11cb2257214b239f
Cabre <cabre94@hotmail.com>
2020-08-17 01:33:53 -0300

    No me acuerdo que modifique

--- Practica_0/ej_1.py
+++ Practica_0/ej_1.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+16-08-2020
+File: ej_1.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+
+Description: 
+"""
+
+import os
 import numpy as np
 
 A = np.array([[ 1,  0,  1],
@@ -12,4 +26,4 @@ c = np.array([-2, 1,-1])
 
 res = np.array([1, -2, -3])
 
-# x = 1  y = -2     z = -3
+# x = 1	y = -2	z = -3
