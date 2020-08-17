1dd67a7a3ee297ee36448f7b8a3346ad8392d7c7
Cabre <cabre94@hotmail.com>
2020-08-17 15:24:02 -0300

    Agrego ejercicio 9

---
+++ Practica_0/ej_9.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+17-08-2020
+File: ej_9.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+"""
+
+import p0_lib
+from p0_lib import rectangulo
+from p0_lib.circunferencia import PI, area
+from p0_lib.elipse import area
+from p0_lib.rectangulo import area as area_rect
