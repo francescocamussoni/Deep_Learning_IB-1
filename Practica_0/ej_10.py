05d0c8608822431da95db8460bcd7652bd172567
Cabre <cabre94@hotmail.com>
2020-08-17 17:17:06 -0300

    Agrego ejercicio 10

---
+++ Practica_0/ej_10.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+17-08-2020
+File: ej_10.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def f(x, y):
+	return (1 - x / 2 + x ** 5 + y ** 3) * np.exp(-x ** 2 - y ** 2)
+
+n = 10
+x = np.linspace(-3, 3, 4 * n)
+y = np.linspace(-3, 3, 3 * n)
+X, Y = np.meshgrid(x,y)
+
+plt.axes([0.025, 0.025, 0.95, 0.95])
+plt.imshow(f(X, -Y), cmap='gray')	# Para que me quedara igual que en el enunciado tengo que cambiar el eje y
+plt.colorbar(shrink=.92)
+
+
+
+plt.xticks(())
+plt.yticks(())
+plt.show()
