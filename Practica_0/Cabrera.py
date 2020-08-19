68e4504a35511820e561c4f3c05493e6aed67837
Cabre <cabre94@hotmail.com>
2020-08-19 18:06:39 -0300

    Agrego los ejercicios 1, 2 y 3

--- Practica_0/Cabrera.py
+++ Practica_0/Cabrera.py
@@ -14,9 +14,9 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-#------------------------
-#		Ejercicio 1
-#------------------------
+#---------------------------------
+#			Ejercicio 1
+#---------------------------------
 
 A = np.array([[ 1,  0,  1],
 			  [ 2, -1,  1],
@@ -30,8 +30,53 @@ invA = np.linalg.inv(A)
 
 res = np.dot(invA,b)
 
-print("res = {}".format(res))
+print("Ejercicio 1: res = {}".format(res))
 
-#------------------------
-#		Ejercicio 
-#------------------------
+
+#---------------------------------
+#			Ejercicio 2
+#---------------------------------
+
+from scipy.stats import gamma
+
+shape = 2	# Se suele notar con k
+scale = 3	# Se suele notar con $\theta$
+
+
+data = np.random.gamma(shape,scale,1000)
+
+# Calculo la media y el desvio standard
+mean = data.mean()		# mu = k * $\theta$
+std = data.std()		# sigma² = k * $\theta^2$
+
+
+x = np.linspace(0, data.max(), 1000)
+
+# Para chequear la distribucion, obtengo k y theta a partir de los datos estadisticos y grafico
+# la distribucion gamma con estos parametros.
+k = (mean / std)**2
+theta = (std**2) / mean
+
+dist = gamma(a=k, scale = theta)
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.hist(data, bins=30, alpha=0.8, label="Datos")
+ax.plot(x, dist.pdf(x) *1000, 'r', label="Dist. Gamma")	# Lo desnormalice
+ax.legend(loc="best")
+ax.set_ylabel("Cuentas")
+#fig.savefig('Informe/ej_2.pdf', format='pdf', bbox_inches='tight')
+plt.show()
+
+
+#---------------------------------
+#			Ejercicio 3
+#---------------------------------
+
+def Bhaskara(a,b=0,c=0):
+	return np.roots([a,b,c]) 	# No se si esto es trampa
+
+res = Bhaskara(1,1,1)
+
+print("Ejercicio 3: Las raices de x²+x+1 son:")
+print("x_1 = {}\nx_2 = {}".format(res[0], res[1]))
