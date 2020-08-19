aca94f353cdcc8b56cb83c30f5873e329e072cdf
Cabre <cabre94@hotmail.com>
2020-08-19 18:39:48 -0300

    Agregue comentarios y puse lindo el grafico

--- Practica_0/ej_4.py
+++ Practica_0/ej_4.py
@@ -16,6 +16,14 @@ from matplotlib import pyplot as plt
 
 from ej_3 import Bhaskara
 
+def seteaGrilla():
+	plt.grid(which='major', axis='x', linewidth=0.75, linestyle='-', color='0.75')
+	plt.grid(which='minor', axis='x', linewidth=0.5, linestyle='-', color='0.75')
+	plt.grid(which='major', axis='y', linewidth=0.5, linestyle='-', color='0.75')
+	plt.grid(which='minor', axis='y', linewidth=0.25, linestyle='-', color='0.75')
+
+# Defino una funcion que dados los coeficientes de un polinomio, lo grafica entre -4.5, 4 e indica sus
+# raices si se encuentran en ese rango (y si son reales)
 def grafYRaices(a,b,c):
 	x = np.linspace(-4.5, 4, 1000)
 	p = np.polyval([a,b,c], x)
@@ -28,4 +36,12 @@ def grafYRaices(a,b,c):
 	if r2.imag == 0 and r2 <= 4 and r2 >= -4.5:
 		plt.plot(r2,0,'or')
 		plt.annotate(s=r"$x_2=${:.2f}".format(r2), xy=(r2, 0), xytext=(r2+0.3, 0))
+
+	seteaGrilla()
+	plt.xlabel("x")
+	plt.ylabel(r"y(x) = ${}x^2 +{}x +{}$".format(a,b,c))
+	#plt.title("Ejercicio 4")
+	plt.savefig('Informe/ej_4.pdf', format='pdf', bbox_inches='tight')
 	plt.show()
+
+grafYRaices(-3,2,16)
