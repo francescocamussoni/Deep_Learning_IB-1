2756925c6f02487350c13caff4e2b7896730006a
Cabre <cabre94@hotmail.com>
2020-08-20 18:57:31 -0300

    Agrego comentarios

--- Practica_0/ej_13.py
+++ Practica_0/ej_13.py
@@ -53,29 +53,39 @@ class Pez(object):
 class Cardumen(object):
 	"""docstring for Cardumen"""
 	def __init__(self):
-		self.N = None
+		self.N = 16
+		self.dt = 0.00001
+		self.iteracion = 0
+		self.dim = None
 		self.__maxVel = None
 		self.__maxDist = None
-		self.Peces = np.array([])
+		#self.Peces = np.array([0,0])
+		self.Peces = np.array( [Pez(R2(0,0), R2(0,0)) for i in range(self.N)] ) 
 		self.CM = None 			# Centro del cardumen
 		self.VM = None			# Velocidad media del cardumen
+		self.gif = []
+		#print(self.Peces)
 
 	def CMyVM(self):
 		aux = self.Peces.sum()
-		print(type(aux))
-		self.CM = aux.pos / self.N
-		self.VM = aux.vel / self.N
+		#print(type(aux))
+		self.CM = (aux.pos / self.N)
+		self.VM = (aux.vel / self.N)
 
-	def initialize(self, N, maxVel, maxDist):
-		self.N = N
+	def initialize(self, dim, maxVel, maxDist):
+		self.dim = dim
 		self.__maxVel = maxVel
 		self.__maxDist = maxDist
-		for i in range(N):
+
+		for i in range(self.N):
 			v = random.uniform(0,maxVel)
 			t = 2*np.pi*random.random()
-			pez = Pez(R2(random.uniform(0,40),random.uniform(0,40)) , R2(v*np.cos(t), v*np.sin(t)))
-			self.Peces = np.append(self.Peces, pez)
+			pez = Pez( R2(random.uniform(0,dim), random.uniform(0,dim)) , R2(v*np.cos(t), v*np.sin(t)))
+			self.Peces[i] = pez
+			#self.Peces = np.append(self.Peces, pez)
+
 		self.CMyVM()
+		#print(self.CM.r)
 		#self.CM = self.CM()
 		#self.VM = self.VM()
 
@@ -97,6 +107,10 @@ class Cardumen(object):
 
 	def reglas(self,i):
 		return self.reglaA(i) + self.reglaB(i) + self.reglaC(i)
+		#return self.reglaA(i)
+		#return self.reglaB(i)
+		#return self.reglaC(i)
+		#return self.reglaA(i) + self.reglaB(i)
 
 	def rebote(self,Pez):
 		pass
@@ -106,45 +120,68 @@ class Cardumen(object):
 		DeltaV = np.array([self.reglas(i) for i in range(self.N)])
 		# Modifico las posiciones y velocidades
 		for i in range(self.N):
-			self.Peces[i].pos = self.Peces[i].pos + self.Peces[i].vel
+			self.Peces[i].pos = self.Peces[i].pos + self.Peces[i].vel * self.dt
 			self.Peces[i].vel = self.Peces[i].vel + DeltaV[i]
-			if self.Peces[i].pos.r[0] > 40:
-				self.Peces[i].pos.r[0] = 80 - self.Peces[i].pos.r[0]	# Lo hago rebotar en x
-				self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]		# Lo hago rebotar
-			elif self.Peces[i].pos.r[0] < 0:
-				self.Peces[i].pos.r[0] = - self.Peces[i].pos.r[0]
-				self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]		# Lo hago rebotar
 
-			if self.Peces[i].pos.r[1] > 40:
-				self.Peces[i].pos.r[1] = 80 - self.Peces[i].pos.r[1]	# Lo hago rebotar en x
-				self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]		# Lo hago rebotar
-			elif self.Peces[i].pos.r[1] < 0:
-				self.Peces[i].pos.r[1] = - self.Peces[i].pos.r[1]
-				self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]		# Lo hago rebotar
+			if 		self.Peces[i].pos.r[0] > 40:							# Pared derecha
+					self.Peces[i].pos.r[0] = 80 - self.Peces[i].pos.r[0]
+					self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]	
+			elif self.Peces[i].pos.r[0] < 0:								# Pared izquierda
+					self.Peces[i].pos.r[0] = - self.Peces[i].pos.r[0]
+					self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]
+
+			if self.Peces[i].pos.r[1] > 40:									# Pared de arriba
+					self.Peces[i].pos.r[1] = 80 - self.Peces[i].pos.r[1]
+					self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]
+			elif self.Peces[i].pos.r[1] < 0:								# Pared de abajo
+					self.Peces[i].pos.r[1] = - self.Peces[i].pos.r[1]
+					self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]		# Lo hago rebotar
 			# Limito la velocidad
 			if self.Peces[i].vel.norma() > self.__maxVel:
-				self.Peces[i].vel = (self.Peces[i].vel / self.Peces[i].vel.norma()) * self.__maxVel
+					self.Peces[i].vel = (self.Peces[i].vel / self.Peces[i].vel.norma()) * self.__maxVel
 
-			plt.plot(self.Peces[i].pos.r[0], self.Peces[i].pos.r[1], 'bo')
+			#plt.plot(self.Peces[i].pos.r[0], self.Peces[i].pos.r[1], 'bo')
+		self.CMyVM()
 		
-		plt.xlim(0,40)
-		plt.ylim(0,40)
-		plt.show()
+		#self.plotea()
+		self.iteracion += 1
+		if self.iteracion % 1000 == 0:
+			self.gif.append( [self.plotea()] )
+		#plt.xlim(0,self.dim)
+		#plt.ylim(0,self.dim)
+		#plt.show()
+
+	def plotea(self):
+		X = np.array( [self.Peces[i].pos.r[0] for i in range(self.N)] )
+		Y = np.array( [self.Peces[i].pos.r[1] for i in range(self.N)] )
+		Vx = np.array( [self.Peces[i].vel.r[0] for i in range(self.N)] )
+		Vy = np.array( [self.Peces[i].vel.r[1] for i in range(self.N)] )
+		return plt.quiver(X,Y,Vx,Vy, alpha=0.8)
+		#return plt.plot(X,Y)
 
 	def print(self):
 		for i in range(self.N):
 			print("Pez: {}".format(i), end=' ')
-			print("pos = ({}, {})".format(self.Peces[i].pos.r[0], self.Peces[i].pos.r[1]), end=' ')
-			print("vel = ({}, {})".format(self.Peces[i].vel.r[0], self.Peces[i].vel.r[1]))
+			print("pos = ({:.2f}, {:.2f})".format(self.Peces[i].pos.r[0], self.Peces[i].pos.r[1]), end=' ')
+			print("vel = ({:.2f}, {:.2f})".format(self.Peces[i].vel.r[0], self.Peces[i].vel.r[1]))
 
 
+from matplotlib import animation
 
-
-
+fig = plt.figure()
 		
 		
 c = Cardumen()
-c.initialize(16, 10, 5)
-for i in range(50):
+c.initialize(40, 10, 1)
+
+for i in range(50000):
+	if i % 1000 == 0:
+		print(i)
 	c.doStep()
-	c.print()
+	#c.print()
+
+ani = animation.ArtistAnimation(fig, c.gif, interval=10, blit=True, repeat_delay=1000)
+
+plt.show()
+
+#ani.save("cardumen.mp4")
