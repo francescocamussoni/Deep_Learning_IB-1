8d72c871f6435e3311fcc6c5d2ff548b8029b861
Cabre <cabre94@hotmail.com>
2020-08-21 01:12:47 -0300

    Agrego los ultimos ejercicios

--- Practica_0/Cabrera.py
+++ Practica_0/Cabrera.py
@@ -10,9 +10,10 @@ GitLab: https://gitlab.com/cabre94
 Description: 
 """
 
-import os
 import numpy as np
 from matplotlib import pyplot as plt
+import random
+
 
 #---------------------------------
 #			Ejercicio 1
@@ -161,11 +162,14 @@ def ejercicio_6(a,b,x):
 #			Ejercicio 7
 #---------------------------------
 
-import circunferencia as circle
-from circunferencia import PI, area
-
+#import circunferencia as circle
+#from circunferencia import PI, area
 
 def ejercicio_7():
+
+	import circunferencia as circle
+	from circunferencia import PI, area
+
 	print("Ejercicio 7:")
 	print("Valor de PI importando con el alias \"circle\": {}".format(circle.PI))
 	print("Valor de del area de un circulo de radio 2, importando con el alias \"circle\": {}".format(circle.area(2)))
@@ -223,54 +227,292 @@ def ejercicio_10():
 #---------------------------------
 
 
+def ejercicio_11():
+	n = 256
+	x = np.linspace(-3, 3, n)
+	y = np.linspace(-3, 3, n)
+	X, Y = np.meshgrid(x,y)
+
+	plt.figure()
+	plt.axes([0.025, 0.025, 0.95, 0.95])
+	plt.contourf(X, Y, f(X, Y), 7, alpha=.75, cmap=plt.cm.hot)
+	C = plt.contour(X, Y, f(X, Y), 7, colors='black')
+	plt.clabel(C, inline=5, fontsize=8)
+
+	#plt.title("Ejercicio 11")
+	plt.xticks(())
+	plt.yticks(())
+	#plt.savefig('Informe/ej_11.png', format='png', bbox_inches='tight')
+	plt.show()
 
 #---------------------------------
 #			Ejercicio 12
 #---------------------------------
 
+def ejercicio_12():
+	n = 1024
+	X = np.random.normal(0,1,n)
+	Y = np.random.normal(0,1,n)
+	T = np.arctan2(Y, X)
+	#T = 2 * np.pi * np.random.rand(n)
+
+	plt.axes([0.025, 0.025, 0.95, 0.95])
+	plt.scatter(X, Y, c=T, cmap='jet', alpha=0.5, edgecolors='gray')  	# https://matplotlib.org/tutorials/colors/colormaps.html
+
+	plt.xlim(-1.5, 1.5)
+	plt.xticks(())
+	plt.ylim(-1.5, 1.5)
+	plt.yticks(())
+	#plt.savefig('Informe/ej_12.png', format='png', bbox_inches='tight')
+
+	plt.show()
 
 
 #---------------------------------
 #			Ejercicio 13
 #---------------------------------
 
+DT = 1
+NUM_PECES = 16
+PLOTEA = 1
+NUM_ITERAC = 200
 
+
+class R2(object):
+	"""Representa un punto en R2"""
+	def __init__(self, x,y):
+		self.r = np.array([x,y])
+	def __add__(self, R):
+		aux = R2(self.r[0], self.r[1])
+		aux.r = self.r + R.r
+		return aux
+	def __sub__(self, R):
+		aux = R2(self.r[0], self.r[1])
+		aux.r = self.r - R.r
+		return aux
+	def __truediv__(self,n):
+		aux = R2(self.r[0], self.r[1])
+		aux.r = self.r / n
+		return aux
+	def __mul__(self,n):
+		aux = R2(self.r[0], self.r[1])
+		aux.r = self.r * n
+		return aux
+	def norma(self):
+		return np.linalg.norm(self.r)
+
+class Pez(object):
+	"""Representa un Pez con posicion y velocidad"""
+	def __init__(self, pos, vel):
+		self.pos = pos
+		self.vel = vel
+	def __add__(self,pez):
+		aux = Pez(self.pos, self.vel)
+		aux.pos = self.pos + pez.pos
+		aux.vel = self.vel + pez.vel
+		return aux
+
+class Cardumen(object):
+	"""docstring for Cardumen"""
+	def __init__(self):
+		self.N = NUM_PECES			# Cantidad de peces en el cardumen
+		self.dt = DT 				# Variacion temporal
+		self.iteracion = 0 			# numero de iteraciones
+		self.dim = None				# Dimension de la pecera/cuadro donde estan los peces
+		self.__maxVel = None
+		self.__maxDist = None
+		self.Peces = np.array( [Pez(R2(0,0), R2(0,0)) for i in range(self.N)] ) 
+		self.CM = None 				# Centro del cardumen
+		self.VM = None				# Velocidad media del cardumen
+		self.gif = []
+
+		self.fig = plt.figure()
+
+	def CMyVM(self):
+		aux = self.Peces.sum()
+		self.CM = (aux.pos / self.N)
+		self.VM = (aux.vel / self.N)
+
+	def initialize(self, dim, maxVel, maxDist):
+		self.dim = dim
+		self.__maxVel = maxVel
+		self.__maxDist = maxDist
+
+		for i in range(self.N):
+			v = random.uniform(0,maxVel)
+			t = 2*np.pi*random.random()
+			pez = Pez( R2(random.uniform(0,self.dim), random.uniform(0,self.dim)) , R2(v*np.cos(t), v*np.sin(t)))
+			self.Peces[i] = pez
+
+		self.CMyVM()
+
+		plt.title("Ejercicio 13 - Cardumen - {} peces".format(self.N))
+		plt.xlim(0,dim)
+		plt.ylim(0,dim)
+		plt.xticks(())
+		plt.yticks(())		
+
+	def reglaA(self,i):
+		return (self.CM - self.Peces[i].pos) / 8
+
+	def reglaB(self,i):
+		aux = R2(0,0)
+		for j in range(self.N):
+			if j != i:
+				dif = self.Peces[i].pos - self.Peces[j].pos
+				d = dif.norma()
+				if d < self.__maxDist:
+					aux = aux + (dif / d)
+		return aux
+
+	def reglaC(self,i):
+		return (self.VM - self.Peces[i].vel) / 8
+
+	def reglas(self,i):
+		return self.reglaA(i) + self.reglaB(i) + self.reglaC(i)
+
+	def doStep(self):
+		# Calculo los delta de velocidad de cada pez
+		DeltaV = np.array([self.reglas(i) for i in range(self.N)])
+		
+		for i in range(self.N):
+			# Modifico las posiciones y velocidades
+			self.Peces[i].pos = self.Peces[i].pos + self.Peces[i].vel * self.dt
+			self.Peces[i].vel = self.Peces[i].vel + DeltaV[i]
+
+			if 		self.Peces[i].pos.r[0] > self.dim:						# Pared derecha
+					self.Peces[i].pos.r[0] = 2*self.dim - self.Peces[i].pos.r[0]
+					self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]	
+			elif self.Peces[i].pos.r[0] < 0:								# Pared izquierda
+					self.Peces[i].pos.r[0] = - self.Peces[i].pos.r[0]
+					self.Peces[i].vel.r[0] = -self.Peces[i].vel.r[0]
+
+			if self.Peces[i].pos.r[1] > self.dim:							# Pared de arriba
+					self.Peces[i].pos.r[1] = 2*self.dim - self.Peces[i].pos.r[1]
+					self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]
+			elif self.Peces[i].pos.r[1] < 0:								# Pared de abajo
+					self.Peces[i].pos.r[1] = - self.Peces[i].pos.r[1]
+					self.Peces[i].vel.r[1] = -self.Peces[i].vel.r[1]		# Lo hago rebotar
+
+			# Limito la velocidad
+			if self.Peces[i].vel.norma() > self.__maxVel:
+					self.Peces[i].vel = (self.Peces[i].vel / self.Peces[i].vel.norma()) * self.__maxVel
+
+		# Actualizo el centro de masa y la velovidad media
+		self.CMyVM()
+		
+		self.iteracion += 1
+		if self.iteracion % PLOTEA == 0:
+			self.gif.append( [self.plotea()] )
+
+	def plotea(self):
+		X  = np.array( [self.Peces[i].pos.r[0] for i in range(self.N)] )
+		Y  = np.array( [self.Peces[i].pos.r[1] for i in range(self.N)] )
+		Vx = np.array( [self.Peces[i].vel.r[0] for i in range(self.N)] )
+		Vy = np.array( [self.Peces[i].vel.r[1] for i in range(self.N)] )
+		return plt.quiver(X,Y,Vx,Vy)
+
+	def print(self):
+		for i in range(self.N):
+			print("Pez: {}".format(i), end=' ')
+			print("pos = ({:.2f}, {:.2f})".format(self.Peces[i].pos.r[0], self.Peces[i].pos.r[1]), end=' ')
+			print("vel = ({:.2f}, {:.2f})".format(self.Peces[i].vel.r[0], self.Peces[i].vel.r[1]))
+		print("")
+
+
+from matplotlib import animation
+
+def ejercicio_13(dim,vMax,dMax):
+	c = Cardumen()
+	c.initialize(dim,vMax,dMax)
+
+	for i in range(NUM_ITERAC):
+		c.doStep()
+		c.print()
+
+	ani = animation.ArtistAnimation(c.fig, c.gif, interval=50, blit=True, repeat_delay=1000)
+
+	plt.show()
+
+	#ani.save("cardumen_1.mp4")
 
 #---------------------------------
 #			Ejercicio 14
 #---------------------------------
 
+def ejercicio_14():
+	n = np.arange(10,61,5)
+	coincidences = np.array([])
 
+	for i in n:
+		count = 0
+		for _ in range(1000):
+			bd = np.random.randint(1,366,i)		# Genero un array de i elementos con valores enteros entre 1-365
+			c , r = np.unique(bd, return_counts=True)	# esta funcion devuelve los valore ordenados y cant repeticiones
+			if r.size != i:
+				count += 1
+		coincidences = np.append(coincidences, (count/1000)*100)
+
+	plt.title("Ejercicio 14")
+	plt.xlabel("# de personas en el grupo")
+	plt.ylabel(r"$\%$")
+	plt.plot(n, coincidences, 'bo')
+	#plt.savefig('Informe/ej_14.svg', format='svg', bbox_inches='tight')
+	plt.show()
 
 
 #---------------------------------
 #			Ejercicio 15
 #---------------------------------
 
+class Noiser(object):
+	"""docstring for Noiser"""
+	def __init__(self, minV, maxV):
+		self.minV = minV
+		self.maxV = maxV
+	def __call__(self,x):
+		return x + np.random.uniform(self.minV,self.maxV)
+		
+def ejercicio_15(minV,maxV):
+	v_Noiser = np.vectorize(Noiser(minV,maxV))
+
+	a = np.zeros(50)
+
+	print("Ejercicio 15: minV = {} maxV = {}".format(minV,maxV))
+	print(v_Noiser(a))
 
 
 
 
 
-
-
-
-
-
-
+#---------------------------------
+#
+#---------------------------------
 
 if __name__ == '__main__':
 
-	ejercicio_1()
+	#ejercicio_1()
 
-	ejercicio_2()
+	#ejercicio_2()
 
-	ejercicio_3(1,1,1)
+	#ejercicio_3(1,1,1)
 
-	ejercicio_4(-3,2,16)
+	#ejercicio_4(-3,2,16)
 
-	ejercicio_5(5,3,2)
+	#ejercicio_5(5,3,2)
 
-	ejercicio_6(5,3,2)
+	#ejercicio_6(5,3,2)
 
-	ejercicio_7()
+	#ejercicio_7()
+
+	#ejercicio_10()
+
+	#ejercicio_11()
+
+	#ejercicio_12()
+
+	#ejercicio_13(40,4,1)
+
+	#ejercicio_14()
+
+	#ejercicio_15(-1,1)
