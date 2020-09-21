775a43f2d727e81ba4fb130e0a3129340ab6f1f4
Cabre <cabre94@hotmail.com>
2020-09-21 18:24:35 -0300

    Plantilla para optimizadores

--- Practica_2/myModules/optimizers.py
+++ Practica_2/myModules/optimizers.py
@@ -14,3 +14,29 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
+class Optimizer(object):
+    def __init__(self, lr):
+        self.lr = lr
+    
+    def __call__(self, X, Y, model):
+        pass
+
+    def update_weights(self, W, gradW):
+        pass
+
+
+class SGD(Optimizer):
+    def __init__(self, lr, bs=False):
+        super().__init__(lr)
+        self.bs = bs
+    
+    def __call__(self, X, Y, model):
+        if(not self.bs):
+            # No hacer los bacths ndafeabu
+            model.backward(X,Y)
+        else:
+            # Aca si lo tengo que separar
+            model.backward(X,Y)
+    
+    def update_weights(self, W, gradW):
+        pass
