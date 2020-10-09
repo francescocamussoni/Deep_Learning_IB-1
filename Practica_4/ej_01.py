3309b3422e2061d252f7b21128fa36e0cbb8d830
Cabre <cabre94@hotmail.com>
2020-10-09 11:50:57 -0300

    Added exercises 1,2

---
+++ Practica_4/ej_01.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 07-10-2020
+File: ej_01.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as snn
+snn.set(font_scale=1)
+
+from sklearn.datasets import load_boston
+from tensorflow import keras
+
+boston = load_boston()
+# print(boston.DESCR) # Para printear la descripcion del dataset
+
+dim = len(boston["feature_names"])
+
+idx = np.arange(506)
+np.random.shuffle(idx)
+
+x_train = boston["data"][idx[:-len(idx) // 4]]
+x_test = boston["data"][idx[-len(idx) // 4:]]
+
+y_train = boston["target"][idx[:-len(idx) // 4]]
+y_test = boston["target"][idx[-len(idx) // 4:]]
+
+media = x_train.mean(axis=0)
+sigma = x_train.std(axis=0)
+
+x_train = x_train - media
+x_train /= sigma  # x_train /= 255
+x_test = x_test - media
+x_test /= sigma
+
+# plt.plot(x_train, y_train, '.b')
+# plt.plot(x_test,  y_test,  '.r')
+# plt.show()
+
+# Con model
+inputs = keras.layers.Input(shape=x_train.shape[1], name="Input")
+ol = keras.layers.Dense(1, name="Output")(inputs)
+
+model = keras.models.Model(inputs=inputs, outputs=ol, name="LinearRegression")
+
+model.summary()
+
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+
+model.compile(optimizer, loss=keras.losses.MSE, metrics=["mse"])
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs=200,
+                    validation_data=(x_test, y_test),
+                    verbose=2)
+
+y_pred = model.predict(x_test)
+
+# plt.plot(y_pred, y_test, "ob", label="Predicciones")
+# plt.plot(y_test, y_test, "k", label="Target")
+# plt.xlabel("Precios reales [k$]",fontsize=15)
+# plt.ylabel("Precios predichos [k$]",fontsize=15)
+# # plt.axis("equal")
+# plt.savefig("Figuras/1.pdf", format="pdf", bbox_inches="tight")
+# plt.show()
+
+plt.plot(history.history['loss'], label="Loss Training")
+plt.plot(history.history['val_loss'], label="Loss Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Loss", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+# plt.axis("equal")
+plt.savefig("Figuras/2_Loss.pdf", format="pdf", bbox_inches="tight")
+plt.show()
+
+plt.plot(history.history['accuracy'], label="Acc. Training")
+plt.plot(history.history['val_accuracy'], label="Acc. Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Accuracy", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+# plt.axis("equal")
+plt.savefig("Figuras/2_Acc.pdf", format="pdf", bbox_inches="tight")
+plt.show()
+
+# Con Sequential, para practicar
+
+from tensorflow.keras.utils import plot_model
+
+plot_model(model, show_shapes=True, to_file="model.png")
---
+++ Practica_4/ej_02_b.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 07-10-2020
+File: ej_02.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+import seaborn as snn
+
+snn.set(font_scale=1)
+
+from tensorflow.keras.datasets import cifar10
+from tensorflow import keras
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Hago el flatten de los datos
+x_train = x_train.reshape(len(x_train), x_train[0].size).astype(np.float)
+x_test = x_test.reshape(len(x_test), x_test[0].size).astype(np.float)
+y_train = y_train.reshape(y_train.size)
+y_test = y_test.reshape(y_test.size)
+
+# def kk(y):
+#     res = np.zeros(shape=(y.shape[0], 10))
+#     res[np.arange(y.shape[0]), y] = 1
+#     return res
+
+# y_train = kk(y_train)
+# y_test = kk(y_test)
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+# Resto la media y divido por sigma
+media = x_train.mean(axis=0)
+sigma = x_train.std(axis=0)
+
+x_train = x_train - media
+x_train /= sigma  # x_train /= 255
+x_test = x_test - media
+x_test /= sigma
+
+# Input
+inputs = keras.layers.Input(shape=(x_train.shape[1], ), name="Input")
+
+l1 = keras.layers.Dense(100,
+                        name='Hidden_1',
+                        activation=keras.activations.relu,
+                        use_bias=True,
+                        kernel_regularizer=keras.regularizers.l2(5e-3),
+                        kernel_initializer=keras.initializers.RandomUniform(
+                            minval=-5e-2, maxval=5e-2, seed=None))(inputs)
+
+output = keras.layers.Dense(
+    10,
+    name='Output',
+    use_bias=True,
+    activation=keras.activations.linear,
+    kernel_regularizer=keras.regularizers.l2(5e-3),
+    kernel_initializer=keras.initializers.RandomUniform(minval=-5e-2,
+                                                        maxval=5e-2,
+                                                        seed=None))(l1)
+
+model = keras.models.Model(inputs=inputs,
+                           outputs=output,
+                           name='Ejercicio_3_4_o_5')
+
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+
+
+model.compile(
+    optimizer=optimizer,
+    loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+    # loss=keras.losses.categorical_crossentropy,
+    # loss='categorical_crossentropy',
+    # loss='mse',
+    #   loss=keras.losses.binary_crossentropy,
+    #   metrics=[keras.metrics.BinaryAccuracy()])
+    #   metrics=[keras.metrics.Accuracy()])
+    metrics=['accuracy'])
+#   loss=keras.losses.categorical_crossentropy,
+# metrics=keras.metrics.Accuracy())
+
+model.summary()
+
+history = model.fit(x_train,
+                    y_train,
+                    epochs=300,
+                    validation_data=(x_test, y_test),
+                    batch_size=50,
+                    verbose=2)
+
+y_pred = model.predict(x_test)
+
+model.save('Modelo_ej2_Relu_Lin_Softmax.h5')
+np.save("History_2_b_Softmax.npy", history.history)
+
+plt.plot(history.history['loss'], label="Loss Training")
+plt.plot(history.history['val_loss'], label="Loss Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Loss", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+# plt.axis("equal")
+plt.savefig("Figuras/2_Loss_Softmax.pdf", format="pdf", bbox_inches="tight")
+plt.show()
+
+plt.plot(history.history['accuracy'], label="Acc. Training")
+plt.plot(history.history['val_accuracy'], label="Acc. Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Accuracy", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+# plt.axis("equal")
+plt.savefig("Figuras/2_Acc_Softmax.pdf", format="pdf", bbox_inches="tight")
+plt.show()
