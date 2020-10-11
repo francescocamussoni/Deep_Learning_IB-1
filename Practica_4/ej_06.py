460e0b1d7c3b2cd09fac34d17002cf794f06d7c7
Cabre <cabre94@hotmail.com>
2020-10-10 23:16:19 -0300

    Exercise 6 added

---
+++ Practica_4/ej_06.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 10-10-2020
+File: ej_06.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
