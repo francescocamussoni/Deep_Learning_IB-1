786d95ef7977aaa031c1cc248811f1492902ce0d
Cabre <cabre94@hotmail.com>
2020-10-12 02:31:50 -0300

    added Graphics

--- Practica_4/ej_06.py
+++ Practica_4/ej_06.py
@@ -121,7 +121,7 @@ model.summary()
 data_folder = os.path.join('Datos', '5')
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
-model.save_weights('modelo_din_entrenar.h5')
+model.save_weights('modelo_SIN_entrenar.h5')
 
 # 5-folding de los datos
 kf = KFold(n_splits=5, shuffle=True)
@@ -130,13 +130,18 @@ kf = KFold(n_splits=5, shuffle=True)
 idx = np.arange(x.shape[0])
 # check = np.array([])
 
+acc_test = np.array([])
+acc_train = np.array([])
+loss_test = np.array([])
+loss_train = np.array([])
+
 for train_index, test_index in kf.split(idx):
 
     x_train, x_test = x[train_index], x[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
     # Cargo los pesos del modelo sin entrenar
-    model.load_weights('model.h5')
+    model.load_weights('modelo_SIN_entrenar.h5')
 
     # Entreno
     history = model.fit(x_train,
@@ -145,8 +150,101 @@ for train_index, test_index in kf.split(idx):
                         epochs=epochs,
                         batch_size=batch_size,
                         verbose=2)
+    
+    acc_test = np.concatenate((acc_test, history.history['val_Acc'] ))
+    acc_train = np.concatenate((acc_train, history.history['Acc'] ))
+
+    loss_test = np.concatenate((loss_test, history.history['loss'] ))
+    loss_train = np.concatenate((loss_train, history.history['val_loss'] ))
+
+    # acc_test = np.vstack( (acc_test, history.history['val_Acc']) )
+
 
     # kkparachequear = idx[test_index]
     # check = np.append(check, kkparachequear)
+
+acc_test = acc_test.reshape( (5, epochs) )
+acc_train = acc_train.reshape( (5, epochs) )
+loss_test = acc_train.reshape( (5, epochs) )
+loss_train = acc_train.reshape( (5, epochs) )
+
+def toDictionary(va, a, vl, l):
+
+    res ={'test_min' : va.min(axis=0),
+    'test_max' : va.max(axis=0),
+    'test_mean' : va.mean(axis=0),
+
+    'train_min' : a.min(axis=0),
+    'train_max' : a.max(axis=0),
+    'train_mean' : a.mean(axis=0),
+
+    'ltest_min' : vl.min(axis=0),
+    'ltest_max' : vl.max(axis=0),
+    'ltest_mean' : vl.mean(axis=0),
+
+    'ltrain_min' : l.min(axis=0),
+    'ltrain_max' : l.max(axis=0),
+    'ltrain_mean' : l.mean(axis=0)}
+
+    return res
+
+results = toDictionary(acc_test, acc_train, loss_test, loss_train)
+
+# Guardo las imagenes
+img_folder = os.path.join('Figuras', '6')
+if not os.path.exists(img_folder):
+    os.makedirs(img_folder)
+
+xx = np.arange(epochs)
+plt.fill_between(xx, results['train_min'], results['train_max'], alpha=0.2)
+plt.plot(results['train_mean'], '-')
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Accuracy", fontsize=15)
+# plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(
+    img_folder,
+    'Acc_lr={}_rf={}_e={}_bs={}.pdf'.format(lr, rf, epochs,
+                                                   batch_size)),
+            format="pdf",
+            bbox_inches="tight")
+plt.show()
+# plt.close()
+
+# Grafico
+plt.fill_between(xx, results['test_min'], results['test_max'], alpha=0.2)
+plt.plot(results['test_mean'], '-')
+# plt.plot(history.history['loss'], label="Loss")
+# plt.plot(history.history['val_loss'], label="Loss Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Test Accuracy", fontsize=15)
+# plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(
+    img_folder,
+    'Acc_Test_lr={}_rf={}_e={}_bs={}.pdf'.format(lr, rf, epochs,
+                                                   batch_size)),
+            format="pdf",
+            bbox_inches="tight")
+plt.show()
+# plt.close()
+
+plt.fill_between(xx, results['ltrain_min'], results['ltrain_max'], alpha=0.2)
+plt.plot(results['ltrain_mean'], '-', label="Loss")
+plt.fill_between(xx, ['ltest_min'], ['ltest_max'], alpha=0.2)
+plt.plot(['ltest_mean'], '-', label="Loss Test")
+# plt.plot(history.history['loss'], label="Loss")
+# plt.plot(history.history['val_loss'], label="Loss Test")
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Loss", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(
+    img_folder,
+    'Loss_Regul_lr={}_rf={}_e={}_bs={}.pdf'.format(lr, rf, epochs,
+                                                   batch_size)),
+            format="pdf",
+            bbox_inches="tight")
+plt.close()
 
 # check.sort()    # Si todo esta bien esto deberia tener todos los enteros de 0 a x.shape sin repetir
