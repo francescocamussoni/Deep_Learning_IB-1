698d341affe3052a0ea28f87246d02707f570a69
Cabre <cabre94@hotmail.com>
2020-10-17 22:05:06 -0300

    Add makedir and formatting code

--- Practica_4/ej_01.py
+++ Practica_4/ej_01.py
@@ -13,8 +13,6 @@ Description:
 import os
 import numpy as np
 from matplotlib import pyplot as plt
-import seaborn as snn
-snn.set(font_scale=1)
 
 from sklearn.datasets import load_boston
 from tensorflow import keras
@@ -28,24 +26,20 @@ idx = np.arange(506)
 np.random.shuffle(idx)
 
 x_train = boston["data"][idx[:-len(idx) // 4]]
-x_test = boston["data"][idx[-len(idx) // 4:]]
-
 y_train = boston["target"][idx[:-len(idx) // 4]]
+x_test = boston["data"][idx[-len(idx) // 4:]]
 y_test = boston["target"][idx[-len(idx) // 4:]]
 
+# Centramos y normalizamos los datos
 media = x_train.mean(axis=0)
 sigma = x_train.std(axis=0)
 
 x_train = x_train - media
-x_train /= sigma  # x_train /= 255
+x_train /= sigma
 x_test = x_test - media
 x_test /= sigma
 
-# plt.plot(x_train, y_train, '.b')
-# plt.plot(x_test,  y_test,  '.r')
-# plt.show()
-
-# Con model
+# Con models.Model
 inputs = keras.layers.Input(shape=x_train.shape[1], name="Input")
 ol = keras.layers.Dense(1, name="Output")(inputs)
 
@@ -63,14 +57,30 @@ history = model.fit(x_train,
                     validation_data=(x_test, y_test),
                     verbose=2)
 
+# Guardo los datos
+data_folder = os.path.join('Datos', '1')
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+model.save(os.path.join(data_folder, '1_model.h5'))
+np.save(os.path.join(data_folder, '1_history.npy'), history.history)
+
+# Prediccion para los datos de test
 y_pred = model.predict(x_test)
+
+# Grafico y guardo figuras
+img_folder = os.path.join('Figuras', '1')
+if not os.path.exists(img_folder):
+    os.makedirs(img_folder)
 
 plt.plot(y_test, y_pred, "ob", label="Predicciones")
 plt.plot(y_test, y_test, "k", label="Target")
-plt.xlabel("Precios reales [k$]",fontsize=15)
-plt.ylabel("Precios predichos [k$]",fontsize=15)
-# plt.axis("equal")
-plt.savefig("Figuras/1.png", format="png", bbox_inches="tight")
+plt.xlabel("Precios reales [k$]", fontsize=15)
+plt.ylabel("Precios predichos [k$]", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(img_folder, '1.png'),
+            format="png",
+            bbox_inches="tight")
 plt.show()
 
 plt.plot(history.history['loss'], label="Loss Training")
@@ -79,8 +89,9 @@ plt.xlabel("Epocas", fontsize=15)
 plt.ylabel("Loss", fontsize=15)
 plt.legend(loc='best')
 plt.tight_layout()
-# plt.axis("equal")
-plt.savefig("Figuras/2_Loss.png", format="png", bbox_inches="tight")
+plt.savefig(os.path.join(img_folder, '1_Loss.png'),
+            format="png",
+            bbox_inches="tight")
 plt.show()
 
 plt.plot(history.history['mse'], label="Acc. Training")
@@ -89,12 +100,7 @@ plt.xlabel("Epocas", fontsize=15)
 plt.ylabel("Accuracy", fontsize=15)
 plt.legend(loc='best')
 plt.tight_layout()
-# plt.axis("equal")
-plt.savefig("Figuras/2_Acc.png", format="png", bbox_inches="tight")
+plt.savefig(os.path.join(img_folder, '1_Acc.png'),
+            format="png",
+            bbox_inches="tight")
 plt.show()
-
-# Con Sequential, para practicar
-
-# from tensorflow.keras.utils import plot_model
-
-# plot_model(model, show_shapes=True, to_file="model.png")
