e8a8f096bbd69244171cc0439f159d023cfab602
Cabre <cabre94@hotmail.com>
2020-10-18 05:07:42 -0300

    Minor changes

--- Practica_4/ej_02_XOR_a.py
+++ Practica_4/ej_02_XOR_a.py
@@ -13,14 +13,19 @@ Description:
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+
+# Script propio para pasar argumentos por linea de comandos
+from CLArg import lr, rf, epochs, batch_size, description
+
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import metrics, layers, optimizers, losses, activations
+from tensorflow.keras import layers, activations, regularizers
+from tensorflow.keras import losses, metrics, optimizers
+from tensorflow.keras.utils import multi_gpu_model
 
 # Datos que no son datos
 x_train = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
 y_train = np.array([[1], [-1], [-1], [1]])
-y_train = y_train.reshape(y_train.size, 1)
 
 # Arquitectura de la red
 inputs = layers.Input(shape=(x_train.shape[1], ), name="Input")
@@ -40,50 +45,56 @@ model = keras.models.Model(inputs=inputs,
                            name='XOR_Arquitectura_1')
 
 
+# Defino accuracy para el problema de XOR
 def my_acc(y_true, y_pred):
     acc = tf.reduce_mean(
         tf.cast(tf.less_equal(tf.abs(y_true - y_pred), 0.1), tf.float32))
     return acc
 
 
-model.compile(optimizer=optimizers.SGD(learning_rate=1e-2),
+model.compile(optimizer=keras.optimizers.Adam(lr),
               loss=losses.MSE,
               metrics=[my_acc])
 
+# Creo que hay que usar otra cosa, pero el cluster no esta actualizado
+try:
+    model = multi_gpu_model(model, gpus=2)
+except:
+    print("No hay GPUs")
+
 model.summary()
 
-history = model.fit(x_train, y_train, epochs=10000, verbose=2)
+hist = model.fit(x_train, y_train, epochs=epochs, verbose=2)
 
 # Guardo los datos
-data_folder = os.path.join('Datos', '2')
+data_folder = os.path.join('Datos', '2_XOR_A')
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
-model.save(os.path.join(data_folder, '2_model_XOR_A.h5'))
-np.save(os.path.join(data_folder, '2_history_XOR_A.npy'), history.history)
+model.save(os.path.join(data_folder, '{}.h5'.format(description)))
+np.save(os.path.join(data_folder, '{}.npy'.format(description)), hist.history)
 
 # Guardo las imagenes
-img_folder = os.path.join('Figuras', '2')
+img_folder = os.path.join('Figuras', '2_XOR_A')
 if not os.path.exists(img_folder):
     os.makedirs(img_folder)
 
 # Grafico
-plt.plot(history.history['loss'], label="Loss")
+plt.plot(hist.history['loss'], label="Loss")
 plt.xlabel("Epocas", fontsize=15)
 plt.ylabel("Loss", fontsize=15)
 plt.legend(loc='best')
 plt.tight_layout()
-plt.savefig(os.path.join(img_folder, '2_Loss_XOR_A.png'),
+plt.savefig(os.path.join(img_folder, 'Loss_{}.png'.format(description)),
             format="png",
             bbox_inches="tight")
 plt.close()
 
-plt.plot(history.history['my_acc'], label="Accuracy")
+plt.plot(hist.history['my_acc'], label="Accuracy")
 plt.xlabel("Epocas", fontsize=15)
 plt.ylabel("Accuracy", fontsize=15)
 plt.legend(loc='best')
 plt.tight_layout()
-# plt.axis("equal")
-plt.savefig(os.path.join(img_folder, '2_Acc_XOR_A.png'),
+plt.savefig(os.path.join(img_folder, 'Acc_{}.png'.format(description)),
             format="png",
             bbox_inches="tight")
 plt.close()
