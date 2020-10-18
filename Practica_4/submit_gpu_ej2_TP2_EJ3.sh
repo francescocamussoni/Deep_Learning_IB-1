2cdb990aee69fdbf286ca6d2d35a98fee65bc7a3
Cabre <cabre94@hotmail.com>
2020-10-18 05:16:43 -0300

    Script to run .py i n cluster

---
+++ Practica_4/submit_gpu_ej2_TP2_EJ3.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#$ -N output_2_TP2_EJ3
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+##  pido la cola gpu.q
+#$ -q gpu
+## pido una placa
+#$ -l gpu=1
+#$ -l memoria_a_usar=1G
+#
+# Load gpu drivers and conda
+module load miniconda
+
+source activate deep_learning
+
+# Execute the script
+hostname
+
+python ej_02_3.py -lr 1e-5 -rf 1e-1 -e 300 -bs 128
+python ej_02_3.py -lr 1e-5 -rf 1e-2 -e 300 -bs 128
+python ej_02_3.py -lr 1e-5 -rf 1e-3 -e 300 -bs 128
+python ej_02_3.py -lr 1e-5 -rf 0    -e 300 -bs 128
+
+python ej_02_3.py -lr 1e-4 -rf 1e-1 -e 300 -bs 128
+python ej_02_3.py -lr 1e-4 -rf 1e-2 -e 300 -bs 128
+python ej_02_3.py -lr 1e-4 -rf 1e-3 -e 300 -bs 128
+python ej_02_3.py -lr 1e-4 -rf 0    -e 300 -bs 128
+
+python ej_02_3.py -lr 1e-3 -rf 1e-1 -e 300 -bs 128
+python ej_02_3.py -lr 1e-3 -rf 1e-2 -e 300 -bs 128
+python ej_02_3.py -lr 1e-3 -rf 1e-3 -e 300 -bs 128
+python ej_02_3.py -lr 1e-3 -rf 0    -e 300 -bs 128
+
+python ej_02_3.py -lr 1e-2 -rf 1e-1 -e 300 -bs 128
+python ej_02_3.py -lr 1e-2 -rf 1e-2 -e 300 -bs 128
+python ej_02_3.py -lr 1e-2 -rf 1e-3 -e 300 -bs 128
+python ej_02_3.py -lr 1e-2 -rf 0    -e 300 -bs 128
+
+python ej_02_3.py -lr 1e-1 -rf 1e-1 -e 300 -bs 128
+python ej_02_3.py -lr 1e-1 -rf 1e-2 -e 300 -bs 128
+python ej_02_3.py -lr 1e-1 -rf 1e-3 -e 300 -bs 128
+python ej_02_3.py -lr 1e-1 -rf 0    -e 300 -bs 128
---
+++ Practica_4/submit_gpu_ej2_TP2_XOR.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+#$ -N output_2_TP2_EJ4
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+##  pido la cola gpu.q
+#$ -q gpu
+## pido una placa
+#$ -l gpu=1
+#$ -l memoria_a_usar=1G
+#
+# Load gpu drivers and conda
+module load miniconda
+
+source activate deep_learning
+
+# Execute the script
+hostname
+
+python ej_02_XOR_a.py -lr 1e-5 -e 10000
+python ej_02_XOR_a.py -lr 1e-4 -e 10000
+python ej_02_XOR_a.py -lr 1e-3 -e 10000
+python ej_02_XOR_a.py -lr 1e-2 -e 10000
+python ej_02_XOR_a.py -lr 1e-1 -e 10000
+python ej_02_XOR_a.py -lr 1e0  -e 10000
+
+python ej_02_XOR_b.py -lr 1e-5 -e 10000
+python ej_02_XOR_b.py -lr 1e-4 -e 10000
+python ej_02_XOR_b.py -lr 1e-3 -e 10000
+python ej_02_XOR_b.py -lr 1e-2 -e 10000
+python ej_02_XOR_b.py -lr 1e-1 -e 10000
+python ej_02_XOR_b.py -lr 1e0  -e 10000
