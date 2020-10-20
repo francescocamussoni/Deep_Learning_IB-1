3d0e1bf4c29bdcdeceeb52565cf34c4b06095daa
Cabre <cabre94@hotmail.com>
2020-10-19 22:20:49 -0300

    Update

--- Practica_4/submit_gpu_ej4_Embedding.sh
+++ Practica_4/submit_gpu_ej4_Embedding.sh
@@ -18,34 +18,33 @@ source activate deep_learning
 # Execute the script
 hostname
 
-#python testkeras.py
-python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 25 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 50  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 75  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 100 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 125 -bs 64 -nn 25
+python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 25 -bs 256 -nn 25			# Mucho overfitting 0.87
+python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 50 -bs 256 -nn 25			# Mas epocas
+#python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 75  -bs 64 -nn 25		# Mas epocas
+#python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 100 -bs 64 -nn 25		# Da mal
+#python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 125 -bs 64 -nn 25		# Da mal
 
-python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 25 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 50  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 75  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 100 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 125 -bs 64 -nn 25
+python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 25 -bs 256 -nn 25			# Mas epocas, el mejorcito
+python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 50 -bs 256 -nn 25			# Mas epocas, el mejorcito
+#python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 75  -bs 64 -nn 25		# Mas epocas
+#python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 100 -bs 64 -nn 25		# Mas epocas
+#python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 125 -bs 64 -nn 25		# Overfitting
 
-python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 25 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 50  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 75  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 100 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 125 -bs 64 -nn 25
+python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 25 -bs 256 -nn 25			# Overfitting
+python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 50 -bs 256 -nn 25		# Overfitting
+#python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 75  -bs 64 -nn 25		# Overfitting
+#python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 100 -bs 64 -nn 25		# Overffiting
+#python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 125 -bs 64 -nn 25		# Mucho overfitting
 
-python ej_04_Embeddings.py -lr 1e-2 -e 200 -ed 25 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-2 -e 200 -ed 50  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-2 -e 200 -ed 75  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-2 -e 200 -ed 100 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-2 -e 200 -ed 125 -bs 64 -nn 25
+#python ej_04_Embeddings.py -lr 1e-2 -e 10 -ed 25 -bs 64 -nn 25			# Raro
+#python ej_04_Embeddings.py -lr 1e-2 -e 10 -ed 50  -bs 64 -nn 25		# Overfitting
+#python ej_04_Embeddings.py -lr 1e-2 -e 10 -ed 75  -bs 64 -nn 25		# Overfitting
+#python ej_04_Embeddings.py -lr 1e-2 -e 10 -ed 100 -bs 64 -nn 25		# Overfitting
+#python ej_04_Embeddings.py -lr 1e-2 -e 10 -ed 125 -bs 64 -nn 25		# Da mal
 
-python ej_04_Embeddings.py -lr 1e-1 -e 200 -ed 25 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-1 -e 200 -ed 50  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-1 -e 200 -ed 75  -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-1 -e 200 -ed 100 -bs 64 -nn 25
-python ej_04_Embeddings.py -lr 1e-1 -e 200 -ed 125 -bs 64 -nn 25
+#python ej_04_Embeddings.py -lr 1e-1 -e 10 -ed 25 -bs 64 -nn 25			# Da mal
+#python ej_04_Embeddings.py -lr 1e-1 -e 10 -ed 50  -bs 64 -nn 25		# Da mal
+#python ej_04_Embeddings.py -lr 1e-1 -e 10 -ed 75  -bs 64 -nn 25		# Da mal
+#python ej_04_Embeddings.py -lr 1e-1 -e 10 -ed 100 -bs 64 -nn 25		# Da mal
+#python ej_04_Embeddings.py -lr 1e-1 -e 10 -ed 125 -bs 64 -nn 25		# Da mal
 
