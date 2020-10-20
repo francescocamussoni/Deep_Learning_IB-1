f80aad68e428effa9729914d3c8ca64ac2c64551
Cabre <cabre94@hotmail.com>
2020-10-19 22:56:08 -0300

    Update

--- Practica_4/submit_gpu_ej4_Embedding.sh
+++ Practica_4/submit_gpu_ej4_Embedding.sh
@@ -20,19 +20,19 @@ hostname
 
 python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 25 -bs 256 -nn 25			# Mucho overfitting 0.87
 python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 50 -bs 256 -nn 25			# Mas epocas
-#python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 75  -bs 64 -nn 25		# Mas epocas
+python ej_04_Embeddings.py -lr 1e-5 -e 200 -ed 75 -bs 256 -nn 25		# Mas epocas
 #python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 100 -bs 64 -nn 25		# Da mal
 #python ej_04_Embeddings.py -lr 1e-5 -e 10 -ed 125 -bs 64 -nn 25		# Da mal
 
 python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 25 -bs 256 -nn 25			# Mas epocas, el mejorcito
 python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 50 -bs 256 -nn 25			# Mas epocas, el mejorcito
-#python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 75  -bs 64 -nn 25		# Mas epocas
+python ej_04_Embeddings.py -lr 1e-4 -e 200 -ed 75 -bs 256 -nn 25		# Mas epocas
 #python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 100 -bs 64 -nn 25		# Mas epocas
 #python ej_04_Embeddings.py -lr 1e-4 -e 10 -ed 125 -bs 64 -nn 25		# Overfitting
 
 python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 25 -bs 256 -nn 25			# Overfitting
 python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 50 -bs 256 -nn 25		# Overfitting
-#python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 75  -bs 64 -nn 25		# Overfitting
+python ej_04_Embeddings.py -lr 1e-3 -e 200 -ed 75 -bs 256 -nn 25		# Overfitting
 #python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 100 -bs 64 -nn 25		# Overffiting
 #python ej_04_Embeddings.py -lr 1e-3 -e 10 -ed 125 -bs 64 -nn 25		# Mucho overfitting
 
