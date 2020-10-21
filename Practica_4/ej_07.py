a49894f7dd428e84ed108cfb32efb76a77fd11e6
Cabre <cabre94@hotmail.com>
2020-10-21 13:46:46 -0300

    Plots

--- Practica_4/ej_07.py
+++ Practica_4/ej_07.py
@@ -52,61 +52,136 @@ model = keras.models.Sequential(name='Autoencoder_Conv')
 model.add(layers.Input(shape=(28, 28, 1)))
 
 # Encoder
+model.add(layers.Conv2D(64, 3, activation=activations.relu, padding='same'))
+model.add(layers.MaxPooling2D())
 model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='same'))
 model.add(layers.MaxPooling2D())
-model.add(layers.Conv2D(16, 3, activation=activations.relu, padding='same'))
-model.add(layers.MaxPooling2D())
-model.add(layers.Conv2D(8, 3, activation=activations.relu, padding='same'))
-model.add(layers.MaxPooling2D(padding='same', name='Hola'))
-
-# model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='same'))
+model.add(layers.MaxPooling2D(padding='same', name='encoder'))
 
 # Decoder
-model.add(layers.Conv2D(8, 3, activation=activations.relu, padding='same'))
+model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='same'))
 model.add(layers.UpSampling2D())
-model.add(layers.Conv2D(16, 3, activation=activations.relu, padding='same'))
+model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='same'))
 model.add(layers.UpSampling2D())
-model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='valid'))
+# model.add(layers.Conv2D(64, 3, activation=activations.relu, padding='same'))
+model.add(layers.Conv2D(64, 3, activation=activations.relu, padding='valid'))
 model.add(layers.UpSampling2D())
+# model.add(layers.BatchNormalization())
 model.add(layers.Conv2D(1, 3, activation=activations.linear, padding='same'))
-
-model.summary()
+model.add(tf.keras.layers.ReLU(max_value=1))
 
 model.compile(
     optimizer=optimizers.Adam(learning_rate=lr),
-    loss=losses.BinaryCrossentropy(from_logits=True, name='loss'),
-    metrics=[metrics.BinaryAccuracy(name='B_Acc'), "accuracy", "mse"])
+    loss=losses.MeanSquaredError(name='loss'),
+    # loss=losses.CategoricalCrossentropy(name='loss'),
+    # loss=losses.BinaryCrossentropy(name='loss'),
+    # loss=losses.BinaryCrossentropy(from_logits=True, name='loss'),
+    metrics=[
+        metrics.BinaryAccuracy(name='B_Acc'),
+        metrics.MeanSquaredError(name='MSE'),
+        #   metrics.CategoricalAccuracy(name='NoEntiendo'),
+        #   metrics.CategoricalCrossentropy(name='Acc_CCE'),
+        #   metrics.Accuracy(name="Acc2"),
+        #   metrics.MeanSquaredError(name='MSE_acc'),
+        # "accuracy",
+        # "mse"
+    ])
 
-model.predict(x_train[:1])
+# Entreno
+hist = model.fit(
+    x_train_n[:20],
+    x_train[:20],
+    #  validation_data=(x_val_n[:5], x_val[:5]),
+    epochs=1000,
+    batch_size=batch_size,
+    verbose=2)
 
-# TODO Cambiar a test
-n = 1
-f = plt.figure(figsize=(10, 4))
-for i in range(n):
-    # display noisy
-    ax = plt.subplot(3, n, i + 1)
-    plt.imshow(x_train[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+# Calculo la loss y Accuracy para los datos de test
+test_loss, test_Acc, test_MSE = model.evaluate(x_test_n, x_test)
 
-    # display noisy
-    ax = plt.subplot(3, n, i + 1 + n)
-    plt.imshow(x_train_n[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+# Guardo los datos
+data_folder = os.path.join('Datos', '7')
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+model.save(os.path.join(data_folder, '{}.h5'.format(description)))
+np.save(os.path.join(data_folder, '{}.npy'.format(description)), hist.history)
 
-    kk = model.get_layer('Hola').output
+# Tomo un modelo auxiliar para ver el output del encoder
+encoder = keras.models.Model(model.input, model.get_layer('encoder').output)
 
-    import ipdb
-    ipdb.set_trace(context=15)  # XXX BREAKPOINT
+# Tomo un ejemplo para graficar
+eg = np.random.randint(0, x_test_n.shape[0])
 
-    # display reconstruction
-    ax = plt.subplot(3, n, i + 1 + 2 * n)
-    plt.imshow(kk[0].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-# f.savefig('denoisingDigits_dense.png', bbox_inches='tight')
-plt.show()
+predict = model.predict(x_test_n[eg:eg + 1])
+o_encoder = encoder.predict(x_test_n[eg:eg + 1])
+o_encoder = o_encoder[0]
+
+
+# Guardo las imagenes
+img_folder = os.path.join('Figuras', '7')
+if not os.path.exists(img_folder):
+    os.makedirs(img_folder)
+
+plt.figure()
+plt.tight_layout()
+ax = plt.subplot(6, 1, 1)
+plt.imshow(x_test_n[eg].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+for j in range(1, 5):
+    for i in range(8):
+        ax = plt.subplot(6, 8, 8 * j + i + 1)
+        plt.imshow(o_encoder[:, :, 8 * (j - 1) + i].reshape(4, 4))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+ax = plt.subplot(6, 1, 6)
+plt.imshow(predict.reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.savefig(
+    os.path.join(img_folder,
+                 'Grafico_{}.png'.format(description),
+                 format="png",
+                 bbox_inches='tight'))
+plt.close()
+
+# Grafico
+plt.plot(hist.history['loss'], label="Loss Training")
+plt.plot(hist.history['val_loss'], label="Loss Validation")
+plt.title("Acc Test: {:.3f}\tMSE: {:.3f}".format(test_Acc, test_MSE))
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Loss", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(img_folder, 'Loss_{}.png'.format(description)),
+            format="png",
+            bbox_inches="tight")
+plt.close()
+
+plt.plot(hist.history['B_Acc'], label="Acc. Training")
+plt.plot(hist.history['val_B_Acc'], label="Acc. Validation")
+plt.title("Acc Test: {:.3f}\tMSE: {:.3f}".format(test_Acc, test_MSE))
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("Accuracy", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(img_folder, 'Acc_{}.png'.format(description)),
+            format="png",
+            bbox_inches="tight")
+plt.close()
+
+plt.plot(hist.history['MSE'], label="MSE Training")
+plt.plot(hist.history['val_MSE'], label="MSE Validation")
+plt.title("Acc Test: {:.3f}\tMSE: {:.3f}".format(test_Acc, test_MSE))
+plt.xlabel("Epocas", fontsize=15)
+plt.ylabel("MSE", fontsize=15)
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig(os.path.join(img_folder, 'MSE_{}.png'.format(description)),
+            format="png",
+            bbox_inches="tight")
+plt.close()
