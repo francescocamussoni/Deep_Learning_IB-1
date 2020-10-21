99ec37996c16cb184e10e3985fc9df7131db2840
Cabre <cabre94@hotmail.com>
2020-10-21 07:40:27 -0300

    Added Arquitecture

--- Practica_4/ej_07.py
+++ Practica_4/ej_07.py
@@ -30,51 +30,57 @@ from tensorflow.keras import losses, metrics, optimizers
 # Separo el train en train y validation
 x_train, x_val, y_train, y_val = train_test_split(x_train,
                                                   y_train,
-                                                  test_size=1/6,
+                                                  test_size=1 / 6,
                                                   stratify=y_train)
 
-# Concateno los datos para que sea mas facil normalizar y agregar ruido
-# x, y = np.hstack((x_train, x_test)), np.hstack((y_train, y_test))
-# x, y = np.vstack((x_train, x_test)), np.hstack((y_train, y_test))
-
-# Normalizo
-x_train = x_train / 255
-x_test = x_test / 255
-x_val = x_val / 255
+# Reshape de la entrada para Convolucionales y normalizacion
+x_train = x_train.reshape((-1, 28, 28, 1)) / 255
+x_test = x_test.reshape((-1, 28, 28, 1)) / 255
+x_val = x_val.reshape((-1, 28, 28, 1)) / 255
 # Agrego ruido Gaussiano con std = 0.5
 x_train_n = x_train + np.random.normal(scale=0.5, size=x_train.shape)
 x_test_n = x_test + np.random.normal(scale=0.5, size=x_test.shape)
 x_val_n = x_val + np.random.normal(scale=0.5, size=x_val.shape)
 # Satuo los valores en el rango [0,1]
-np.clip(x_train_n, 0, 1, out=x_train_n)
-np.clip(x_test_n, 0, 1, out=x_test_n)
-np.clip(x_val_n, 0, 1, out=x_val_n)
-
-
-
-
-
-
-# Separo el train en train y validation
-# x_train, x_val, y_train, y_val = train_test_split(x_train,
-#                                                   y_train,
-#                                                   test_size=1/6,
-#                                                   stratify=y_train)
-
-# Normalizacion y ruido
+x_train_n = np.clip(x_train_n, 0, 1)
+x_test_n = np.clip(x_test_n, 0, 1)
+x_val_n = np.clip(x_val_n, 0, 1)
 
 # Arquitecura con Convolucionales
+model = keras.models.Sequential(name='Autoencoder_Conv')
 
+model.add(layers.Input(shape=(28, 28, 1)))
 
+# Encoder
+model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='same'))
+model.add(layers.MaxPooling2D())
+model.add(layers.Conv2D(16, 3, activation=activations.relu, padding='same'))
+model.add(layers.MaxPooling2D())
+model.add(layers.Conv2D(8, 3, activation=activations.relu, padding='same'))
+model.add(layers.MaxPooling2D(padding='same', name='Hola'))
 
+# model.add(layers.BatchNormalization())
 
+# Decoder
+model.add(layers.Conv2D(8, 3, activation=activations.relu, padding='same'))
+model.add(layers.UpSampling2D())
+model.add(layers.Conv2D(16, 3, activation=activations.relu, padding='same'))
+model.add(layers.UpSampling2D())
+model.add(layers.Conv2D(32, 3, activation=activations.relu, padding='valid'))
+model.add(layers.UpSampling2D())
+model.add(layers.Conv2D(1, 3, activation=activations.linear, padding='same'))
 
+model.summary()
 
+model.compile(
+    optimizer=optimizers.Adam(learning_rate=lr),
+    loss=losses.BinaryCrossentropy(from_logits=True, name='loss'),
+    metrics=[metrics.BinaryAccuracy(name='B_Acc'), "accuracy", "mse"])
 
+model.predict(x_train[:1])
 
-
-# TODO Cambiar a test 
-n = 5
+# TODO Cambiar a test
+n = 1
 f = plt.figure(figsize=(10, 4))
 for i in range(n):
     # display noisy
@@ -85,17 +91,22 @@ for i in range(n):
     ax.get_yaxis().set_visible(False)
 
     # display noisy
-    ax = plt.subplot(3, n, i + 1+ n)
+    ax = plt.subplot(3, n, i + 1 + n)
     plt.imshow(x_train_n[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-#     # display reconstruction
-#     ax = plt.subplot(3, n, i + 1 + 2*n)
-#     plt.imshow(y_test_noisy[i].reshape(28, 28))
-#     plt.gray()
-#     ax.get_xaxis().set_visible(False)
-#     ax.get_yaxis().set_visible(False)
+    kk = model.get_layer('Hola').output
+
+    import ipdb
+    ipdb.set_trace(context=15)  # XXX BREAKPOINT
+
+    # display reconstruction
+    ax = plt.subplot(3, n, i + 1 + 2 * n)
+    plt.imshow(kk[0].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 # f.savefig('denoisingDigits_dense.png', bbox_inches='tight')
 plt.show()
