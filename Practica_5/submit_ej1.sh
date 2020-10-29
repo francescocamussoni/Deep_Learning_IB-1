1ea5a330a8dc8cad835f5e1838253ef0a71249e9
Cabre <cabre94@hotmail.com>
2020-10-29 20:22:19 -0300

    First commit

---
+++ Practica_5/submit_ej1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+#$ -N o_1
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+##  pido la cola gpu.q
+#$ -q gpu
+## pido una placa
+#$ -l gpu=1
+#$ -l memoria_a_usar=1G
+#
+# Load gpu drivers and conda
+module load miniconda
+
+source activate deep_learning
+
+# Execute the script
+hostname
+
+python ej_01.py -lr 1e-3 -rf 1e-3 -e 50 -bs 256 --small True
+#python ej_01.py -lr 1e-5 -rf 1e-1 -e 300 -bs 128
+#python ej_01.py -lr 1e-5 -rf 1e-1 -e 300 -bs 128
