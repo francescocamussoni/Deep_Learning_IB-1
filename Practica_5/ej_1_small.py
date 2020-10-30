7c18b546cda702bc9c5332b4a4990b1c8e04a188
Cabre <cabre94@hotmail.com>
2020-10-30 12:12:57 -0300

    Minor changes

--- Practica_5/ej_1_small.py
+++ Practica_5/ej_1_small.py
@@ -30,53 +30,58 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-#small_dataset = True
-#path_data = "/home/cabre1994/Desktop/Deep_Learning/Deep_Learning_IB/Datasets"
+# Path del cluster
+path_data = os.path.join("/", "share", "apps", "DeepLearning", "Datos")
+
+# Path local
 path_data = os.getcwd()
 
-if dogs_cats == 'small':
-    save_images = os.path.join(path_data, "dogs-vs-cats_small.npy")
-    save_label = os.path.join(path_data, "dogs-vs-cats_small_label.npy")
-    path_data = os.path.join(path_data, "dogs-vs-cats_small")
-else:
-    save_images = os.path.join(path_data, "dogs-vs-cats.npy")
-    save_label = os.path.join(path_data, "dogs-vs-cats_label.npy")
-    path_data = os.path.join(path_data, "dogs-vs-cats")
+path_file = os.path.join(path_data, "dogs-vs-cats")
 
-i = 1
+files = os.listdir(path_data)
+labels = np.array([],dtype=np.int)
 
-if not os.path.exists(save_images):
-    images = np.array([],dtype=np.uint8)
-    labels = np.array([],dtype=np.uint8)
-
-    for file in os.listdir(path_data):
-        print(i)
-        i += 1
-        #if i > 100:
-        #    continue
-        
-        if dogs_cats == 'small':
-            img = load_img(os.path.join(path_data,file))
-        else:
-            img = load_img(os.path.join(path_data,file), target_size=(299,299,3),interpolation="bilinear")
-
-        img_arr = img_to_array(img).astype(np.uint8)
-
-        images = np.append(images, img_arr)
-
-        if file.startswith('cat'):
+for file in files:
+    if file.startswith('cat'):
             labels = np.append(labels, 0)
         else:
             labels = np.append(labels, 1)
 
-    if dogs_cats == 'small':
-        images = images.reshape(-1,32,32,3)
-        np.save(save_images, images)
-        np.save(save_label, labels)
-    else:
-        images = images.reshape(-1,299,299,3)
-        np.save(save_images, images)
-        np.save(save_label, labels)
+
+
+
+#if not os.path.exists(save_images):
+#    images = np.array([],dtype=np.uint8)
+#    labels = np.array([],dtype=np.uint8)
+#
+#    for file in os.listdir(path_data):
+#        print(i)
+#        i += 1
+#        #if i > 100:
+#        #    continue
+#        
+#        if dogs_cats == 'small':
+#            img = load_img(os.path.join(path_data,file))
+#        else:
+#            img = load_img(os.path.join(path_data,file), target_size=(299,299,3),interpolation="bilinear")
+#
+#        img_arr = img_to_array(img).astype(np.uint8)
+#
+#        images = np.append(images, img_arr)
+#
+#        if file.startswith('cat'):
+#            labels = np.append(labels, 0)
+#        else:
+#            labels = np.append(labels, 1)
+#
+#    if dogs_cats == 'small':
+#        images = images.reshape(-1,32,32,3)
+#        np.save(save_images, images)
+#        np.save(save_label, labels)
+#    else:
+#        images = images.reshape(-1,299,299,3)
+#        np.save(save_images, images)
+#        np.save(save_label, labels)
 
 # Importo los datos
 x_train = np.load(save_images)
